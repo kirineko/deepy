40ff4c48f269bf6982253b4846da3a6e15ac20dc
kirineko <kirineko@qq.com>
2026-05-28 20:15:54 +0800

    Revert "Disable approval panel ANSI cropping"
    This reverts commit 469d10292684c9aa5e119fe3a41840329dab0297.

--- src/deepy/ui/terminal.py
+++ src/deepy/ui/terminal.py
@@ -1155,8 +1155,7 @@ def _approval_panel_ansi(
             project_root=project_root,
             expanded=expanded,
             width=width,
-        ),
-        crop=False,
+        )
     )
     return buffer.getvalue().rstrip("\n")
 
